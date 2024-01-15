79073fcf57700d0ee0f41602b2b4492ce85b20ae
lehtoneo <ossi.j.lehtonen@helsinki.fi>
2024-01-15 10:46:39 +0200

    add bin

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,10 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = (async () => {
-  const defaultConfig = await getDefaultConfig(__dirname);
-  const { assetExts } = defaultConfig.resolver;
-  return {
-    resolver: {
-      // Add bin to assetExts
-      assetExts: [...assetExts, 'bin']
-    }
-  };
-})();
+const config = getDefaultConfig(__dirname);
+
+config.resolver.assetExts.push(
+  // Adds support for `.db` files for SQLite databases
+  'bin'
+);
+
+module.exports = config;
