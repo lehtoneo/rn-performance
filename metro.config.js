4436b9b0246628d6a56c9507b7f08025b5ffe846
lehtoneo <ossi.j.lehtonen@helsinki.fi>
2024-01-15 10:46:04 +0200

    test

--- metro.config.js
+++ metro.config.js
@@ -2,11 +2,11 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 module.exports = (async () => {
-  const defaultConfig = await getDefaultConfig();
+  const defaultConfig = await getDefaultConfig(__dirname);
   const { assetExts } = defaultConfig.resolver;
   return {
     resolver: {
-      // Add bin to assetExts for ts files
+      // Add bin to assetExts
       assetExts: [...assetExts, 'bin']
     }
   };
