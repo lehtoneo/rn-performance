93c1933b1443a1bc9ebda8b7b479eaf10baea23c
lehtoneo <ossi.j.lehtonen@helsinki.fi>
2024-02-26 13:12:20 +0200

    try add typed routers

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,10 @@
+import { Link } from 'expo-router';
 import { Text, View } from 'react-native';
 
 const Test = () => {
   return (
     <View>
+      <Link href={'/'}>Test</Link>
       <Text>Moi</Text>
     </View>
   );
