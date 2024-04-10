aee2a7281a4204eb2a4482c06031828116d8fd1e
lehtoneo <ossi.j.lehtonen@helsinki.fi>
2024-04-10 10:15:42 +0300

    try fix

--- metro.config.js
+++ metro.config.js
@@ -2,10 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.assetExts.push(
-  // Adds support for `.db` files for SQLite databases
-  'bin'
-);
+config.resolver.assetExts.push('bin');
 
 config.resolver.assetExts.push('tflite');
 config.resolver.assetExts.push('onnx');
